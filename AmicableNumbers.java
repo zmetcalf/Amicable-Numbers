5792890a8b1a64d16d82bd82fd0f8d99eac7458d
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-28 22:59:04 -0500

    Problem Solved

--- AmicableNumbers.java
+++ AmicableNumbers.java
@@ -2,15 +2,69 @@ import java.util.ArrayList;
 
 public class AmicableNumbers {
     
-    public AmicableNumbers() {
-        ArrayList<Integer> amicableNumbers = new ArrayList<Integer>();
-        System.out.println(divisorList(30));
-    }
+    private int maxNum;
+    ArrayList<Integer> amicableNumbers = new ArrayList<Integer>();
+    
+    public AmicableNumbers(int maxNum) {
+        this.maxNum = maxNum;
+    } //End Constructor
     
     private static ArrayList<Integer> divisorList (int divisor) {
-        ArrayList<Integer> divisorList = new ArrayList<Integer>();
-            
-        return divisorList;
+        
+        ArrayList<Integer> dList = new ArrayList<Integer>();
+        
+        for(int i = 2; i <= divisor; i++) {
+            if(divisor % i == 0) {
+                dList.add(divisor / i);
+            }
+        }
+        
+        return dList;
+        
+    } //End divisorList method
+    
+    private static int getListTotal (ArrayList<Integer> divisorList) {
+        int total = 0;      
+        
+        for(int divisor : divisorList){
+            total += divisor;
+        }
+        
+        return total;
+    }
+    
+    public void findAmicableNumbers() {
+        clearAmicableNumbers();
+        int iTest = 0;
+        int rTest = 0;
+        for(int i = 2; i <= maxNum; i++) {
+            iTest = getListTotal(divisorList(i));
+            rTest = getListTotal(divisorList(iTest));
+            if(rTest == i && i != iTest) {
+                if(amicableNumbers.contains(i) != true) {
+                    amicableNumbers.add(i);
+                }
+                if(amicableNumbers.contains(rTest) != true) {
+                    amicableNumbers.add(rTest);
+                }
+            }
+        }
+    }
+    
+    public int getSumOfAmicableNumbers() {
+        int total = 0;
+        for(int i : amicableNumbers) {
+            total += i;
+        }
+        return total;
+    }
+    
+    private void clearAmicableNumbers() {
+        amicableNumbers.clear();
+    }
+    
+    public ArrayList<Integer> getAmicableNumbers() {
+        return amicableNumbers;
     }
 
 }
--- AmicableNumbersTest.java
+++ AmicableNumbersTest.java
@@ -1,7 +1,9 @@
 public class AmicableNumbersTest {
     
     public static void main(String [] args) {
-        AmicableNumbers checkNum = new AmicableNumbers();
-        
+        AmicableNumbers checkNum = new AmicableNumbers(10000);
+        checkNum.findAmicableNumbers();
+        System.out.println(checkNum.getSumOfAmicableNumbers());
+        System.out.println(checkNum.getAmicableNumbers());
     }
 }
