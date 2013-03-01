0d7d2ffd595eb585080b7624287b375d1da9fa33
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-28 19:47:54 -0500

    Learning about ArrayLists

--- AmicableNumbers.java
+++ AmicableNumbers.java
@@ -4,7 +4,13 @@ public class AmicableNumbers {
     
     public AmicableNumbers() {
         ArrayList<Integer> amicableNumbers = new ArrayList<Integer>();
-        
+        System.out.println(divisorList(30));
+    }
+    
+    private static ArrayList<Integer> divisorList (int divisor) {
+        ArrayList<Integer> divisorList = new ArrayList<Integer>();
+            
+        return divisorList;
     }
 
 }
--- AmicableNumbersTest.java
+++ AmicableNumbersTest.java
@@ -1,6 +1,7 @@
 public class AmicableNumbersTest {
     
     public static void main(String [] args) {
-
+        AmicableNumbers checkNum = new AmicableNumbers();
+        
     }
 }
